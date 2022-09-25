253f7e57e002bf5853164108fd7cb58236704462
Chris9271 <chrisweng71@outlook.com>
2022-09-25 14:27:37 +0800

    update data css

--- src/components/Data.vue
+++ src/components/Data.vue
@@ -2,8 +2,9 @@
   <div class="l-data">
     <div class="b-data">
       <p class="b-data__title">
-        <span class="b-data__number">13.32</span><br />who seeks after it and
-        wants to have it
+        <span class="b-data__number">13.32</span>
+        <span>who seeks after it and</span>
+        <span>wants to have it</span>
       </p>
       <img src="../img/lighthouse.png" alt="lighthouse" />
     </div>
